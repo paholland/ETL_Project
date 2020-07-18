e7ad5e913f98ef39c55c1c470dec832926592854
bea03013 <63623003+bea03013@users.noreply.github.com>
2020-07-18 11:56:51 -0500

    Add files via upload

---
+++ schemafor Tables.sql
@@ -0,0 +1,101 @@
+-- Create tables for data to be loaded into
+CREATE TABLE passer (
+passId INT,
+playId INT,
+teamId INT,
+playerId INT,
+passPosition TEXT,
+passOutcomes TEXT, 
+passDirection TEXT,
+passDepth TEXT,
+passLength INT,
+passAtt INT,
+passComp INT,
+passTd INT
+);
+
+-- Show that it imported correctly
+SELECT * FROM passer
+
+
+CREATE TABLE draft (
+playerId INT,
+draft INT,
+round FLOAT,
+pick INT,
+draftTradeValue FLOAT,
+draftTeam TEXT,
+position TEXT,
+teamId INT,
+nameFirst TEXT,
+nameLast TEXT,
+nameFull VARCHAR,
+college VARCHAR,
+heightInches FLOAT,
+weight FLOAT,
+dob VARCHAR,
+ageAtDraft FLOAT,
+homeCity VARCHAR,
+homeState TEXT,
+homeCountry TEXT
+);
+
+-- Show that it imported correctly
+SELECT * FROM draft
+
+
+-- Create tables for data to be loaded into
+CREATE TABLE 2016 (
+salaryRk FLOAT,
+position TEXT,
+team TEXT,
+salary VARCHAR,
+player_name VARCHAR
+);
+
+-- Show that it imported correctly
+SELECT * FROM 2016
+
+
+-- Create tables for data to be loaded into
+CREATE TABLE 2017 (
+salaryRk FLOAT,
+position TEXT,
+team TEXT,
+salary VARCHAR,
+player_name VARCHAR
+);
+
+-- Show that it imported correctly
+SELECT * FROM 2017
+
+
+
+-- Create tables for data to be loaded into
+CREATE TABLE 2018 (
+salaryRk FLOAT,
+position TEXT,
+team TEXT,
+salary VARCHAR,
+player_name VARCHAR
+);
+
+-- Show that it imported correctly
+SELECT * FROM 2018
+
+
+
+-- Create tables for data to be loaded into
+CREATE TABLE 2019 (
+salaryRk FLOAT,
+position TEXT,
+team TEXT,
+salary VARCHAR,
+player_name VARCHAR
+);
+
+-- Show that it imported correctly
+SELECT * FROM 2019
+
+
+
