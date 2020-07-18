6f7b89a6d7912c8138039080a7f5f1626059d8cc
EmmanuelAyoo <61052053+EmmanuelAyoo@users.noreply.github.com>
2020-07-18 12:23:19 -0500

    Update schemafor Tables.sql

--- schemafor Tables.sql
+++ schemafor Tables.sql
@@ -3,7 +3,7 @@ CREATE TABLE passer (
 passId INT,
 playId INT,
 teamId INT,
-playerId INT,
+playerId INT PRIMARY KEY,
 passPosition TEXT,
 passOutcomes TEXT, 
 passDirection TEXT,
@@ -19,7 +19,7 @@ SELECT * FROM passer
 
 
 CREATE TABLE draft (
-playerId INT,
+playerId INT FOREIGN KEY,
 draft INT,
 round FLOAT,
 pick INT,
@@ -29,7 +29,7 @@ position TEXT,
 teamId INT,
 nameFirst TEXT,
 nameLast TEXT,
-nameFull VARCHAR,
+nameFull VARCHAR PRIMARY KEY,
 college VARCHAR,
 heightInches FLOAT,
 weight FLOAT,
@@ -46,6 +46,7 @@ SELECT * FROM draft
 
 -- Create tables for data to be loaded into
 CREATE TABLE 2016 (
+id SERIAL PRIMARY KEY, 
 salaryRk VARCHAR,
 position TEXT,
 team TEXT,
@@ -59,11 +60,12 @@ SELECT * FROM 2016
 
 -- Create tables for data to be loaded into
 CREATE TABLE 2017 (
+id SERIAL PRIMARY KEY,  
 salaryRk VARCHAR,
 position TEXT,
 team TEXT,
 salary VARCHAR,
-player_name VARCHAR
+player_name VARCHAR 
 );
 
 -- Show that it imported correctly
@@ -73,11 +75,12 @@ SELECT * FROM 2017
 
 -- Create tables for data to be loaded into
 CREATE TABLE 2018 (
+id SERIAL PRIMARY KEY,
 salaryRk VARCHAR,
 position TEXT,
 team TEXT,
 salary VARCHAR,
-player_name VARCHAR
+player_name VARCHAR 
 );
 
 -- Show that it imported correctly
@@ -87,6 +90,7 @@ SELECT * FROM 2018
 
 -- Create tables for data to be loaded into
 CREATE TABLE 2019 (
+id SERIAL PRIMARY KEY,
 salaryRk VARCHAR,
 position TEXT,
 team TEXT,
